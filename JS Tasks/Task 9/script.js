dd6eee6c3f65b02db857082d3bccb90319a1bcff
EglePen <egle.penkaityte@gmail.com>
2022-12-08 00:59:22 +0200

    task 9 done

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -9,3 +9,20 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title,
+            this.director = director,
+            this.budget = budget
+    }
+    wasExpensive() {
+        return this.budget > 100000000
+    }
+};
+
+const movieOne = new Movie('Pirates of the Caribbean: On Stranger Tides', 'Rob Marshall', 379000000);
+const movieTwo = new Movie('Cleopatra', 'Joseph L. Mankiewicz', 31000000);
+
+movieOne.wasExpensive(); //true
+movieTwo.wasExpensive(); //false
