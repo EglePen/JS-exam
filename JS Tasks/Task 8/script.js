eeba89b449c78d0aa64b6596f7bb7082766bae6e
EglePen <egle.penkaityte@gmail.com>
2022-12-08 01:01:59 +0200

    task 8 done

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function () {
+        return this.a + this.b;
+    };
+    this.subtraction = function () {
+        return this.a - this.b;
+    };
+    this.multiplication = function () {
+        return this.a * this.b;
+    };
+    this.division = function () {
+        return this.a / this.b;
+    };
+}
+
+const letsCalculate = new Calculator(2, 2);
+letsCalculate.sum(); //4
+letsCalculate.subtraction(); //0
+letsCalculate.multiplication(); //4
+letsCalculate.division(); //1
+
