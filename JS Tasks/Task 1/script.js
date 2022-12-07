cda55a517bb4f9d2aaa3843110ed9b6f00ea67c7
EglePen <egle.penkaityte@gmail.com>
2022-12-07 19:20:19 +0200

    task 1 done

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,24 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const formEl = document.querySelector('form');
+const results = document.querySelector('#output');
+
+formEl.addEventListener('submit', calculate);
+
+function calculate(event) {
+    event.preventDefault();
+    const inputValue = document.getElementById('search').value;
+
+    results.innerHTML = '';
+
+    const convertToLb = inputValue * 2.2046;
+    const convertToGram = inputValue / 0.0010000;
+    const convertToOz = inputValue * 35.274;
+
+    results.innerHTML = 
+        `<div class='output-line'> ${convertToLb} lb</div>
+        <div class='output-line'> ${convertToGram} g</div>
+        <div class='output-line'>${convertToOz} oz</div>`;
+}
