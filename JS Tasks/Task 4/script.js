39284b68c6bbd586c7454e9e465c0771b10114d4
EglePen <egle.penkaityte@gmail.com>
2022-12-08 20:23:24 +0200

    task 4 done

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -7,5 +7,18 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
+import cars from './cars.json' assert {type: 'json'};
 
-const ENDPOINT = 'cars.json';
+const outputDiv = document.querySelector('#output');
+
+function displayCars(car) {
+    car.forEach(element => {
+        outputDiv.innerHTML +=
+        `<div class='brand-card'>
+            <div class='brand'> ${element.brand}</div> 
+            <div class='models'> ${element.models}</div>
+        </div>`
+    });
+}
+
+displayCars(cars);
